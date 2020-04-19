1c6b7f5385951087c58e700fe129620d7965e78b
Ashley <ashleybrookecohen@gmail.com>
2020-04-19 17:00:10 -0400

    Add files via upload

---
+++ tutorial2.py
@@ -0,0 +1,17 @@
+#Tutorial python Lesson 2
+
+#set variable y to the integer 6.
+y=6
+
+#print the variable and its datatype
+print(y+' is ',type(y))
+
+
+
+#all:
+#slicing
+#len()
+
+#strings only:
+#strip,lower,upper,replace,split
+
